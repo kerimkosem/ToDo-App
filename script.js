4155078a1bd58b851a4a2e214cd941ae4b250401
kerim <krmksmsite@gmail.com>
2022-08-21 15:00:41 +0300

    local storage added

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const form = document.querySelector("form")
 const input = document.querySelector("input")
 const list = document.querySelector("#todos")
 
+
+const todoEl = JSON.parse(localStorage.getItem("todos"))
+
+
+if (todoEl) {
+
+    todoEl.forEach(item => addToDo(item))
+
+}
+
 form.addEventListener("submit", (e) => {
 
     e.preventDefault()
@@ -11,13 +21,21 @@ form.addEventListener("submit", (e) => {
 
 })
 
-function addToDo() {
+function addToDo(todo) {
 
     let todoText = input.value
 
-    if(todoText) {
+    if (todo) {
+        todoText = todo.text
+    }
+
+    if (todoText) {
 
         let todoList = document.createElement("li")
+
+        if (todo && todo.completed) {
+            todoList.classList.add("completed")
+        }
 
         todoList.innerText = todoText
 
@@ -28,17 +46,34 @@ function addToDo() {
         todoList.addEventListener("click", () => {
 
             todoList.classList.toggle("completed")
+            addLocale()
 
-        } )
+        })
 
         todoList.addEventListener("contextmenu", (e) => {
-           
+
             e.preventDefault()
 
             todoList.remove()
+
+            addLocale()
         })
+
+        addLocale()
 
     }
 }
 
+
+function addLocale() {
+
+    let todos = document.querySelectorAll("li")
+    
+    let todosList = []
+  
+    todos.forEach(item => todosList.push({ text: item.innerText, completed: item.classList.contains("completed") }))
+    
+    localStorage.setItem("todos", JSON.stringify(todosList))
+
+}
 
